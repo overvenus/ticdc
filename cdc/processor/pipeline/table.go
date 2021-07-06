2abf67efddfbf90ca71e0e21411fdf7c059414b3
Neil Shen <overvenus@gmail.com>
2021-07-06 12:30:57 +0800

    pipeline: reduce tick message in-use objects
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- cdc/processor/pipeline/table.go
+++ cdc/processor/pipeline/table.go
@@ -177,7 +177,7 @@ func NewTablePipeline(ctx cdcContext.Context,
 		zap.Uint64("quota", perTableMemoryQuota))
 	flowController := common.NewTableFlowController(perTableMemoryQuota)
 
-	p := pipeline.NewPipeline(ctx, 500*time.Millisecond)
+	p := pipeline.NewPipeline(ctx, 2*time.Second)
 	p.AppendNode(ctx, "puller", newPullerNode(limitter, tableID, replicaInfo, tableName))
 	p.AppendNode(ctx, "sorter", newSorterNode(tableName, tableID, flowController, mounter))
 	p.AppendNode(ctx, "mounter", newMounterNode())
